c5b2d600293022d826cb8d279e5a5995a70b869b
RS <rami.simons@hotmail.com>
2024-12-01 02:23:05 +0100

    First commit

---
+++ src/components/Word.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let word;
+</script>
+
+<div class="flex flex-col justify-center gap-1 items-center p-1 m-1">
+    <span>{word.dutch}</span>
+    <span>{word.english}</span>
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { onMount } from 'svelte';
+    import { readable, writable } from 'svelte/store';
+    import Word from '../components/Word.svelte';
+
+    let vocab = [
+        { dutch: "Hallo!", english: "Hello!"},
+        { dutch: "Goede-", english: "Good-"},
+        { dutch: "-morgen", english: "-morning"},
+        { dutch: "-middag", english: "-afternoon"},
+        { dutch: "-avond", english: "-evening"},
+        { dutch: "-nacht", english: "-night"},
+    ];
+</script>
+
+<main id="main" class="h-screen content-center bg-red-200">
+
+<section id="" class="w-auto h-[80%] bg-green-200">
+<div id="builder" class="flex flex-row bg-green-600 h-[80%] w-auto px-1">
+
+    <div id="builder-vocab" class="w-[40%] bg-orange-300">
+        {#each vocab as word}
+            <Word {word} />
+        {/each}
+    </div>
+
+    <div id="builder-phrase" class="w-screen bg-red-300">
+    </div>
+
+</div>
+</section>
+
+</main>
